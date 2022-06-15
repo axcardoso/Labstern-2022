e2b239341bd0c57342bbf16fab700469d8d38e8e
axcardoso <107520276+axcardoso@users.noreply.github.com>
2022-06-14 22:02:44 -0300

    Add files via upload

---
+++ EP02.py
@@ -0,0 +1,197 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import random
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy.stats
+
+#Escreva seu nome e numero USP
+INFO = {11820771:"André Luis Cardoso"}
+A = 0.262143264  # A = 0.rg
+B = 0.91936739062  # B = 0.cpf
+
+def f(x):
+    """
+    Esta funcao deve receber x e devolver f(x), como especifcado no enunciado
+    Escreva o seu codigo nas proximas linhas
+    """
+    
+    return np.exp(-A*x)*np.cos(B*x)
+
+def g(x):
+    """
+    Variável de Controle
+    
+    """
+    return -0.6*x + 1.05
+
+def crude(n = 1100000, Seed = None):
+    random.seed(Seed)
+    """
+    Esta funcao deve retornar a sua estimativa para o valor da integral de f(x)
+    usando o metodo crude
+    Escreva o seu codigo nas proximas linhas
+    """
+    
+    soma = 0
+    lista_estimativas = []
+    N = n
+    
+    #gerando pontos uniformemente e aplicando na função
+    for n in range(1,N+1):
+        soma += f(random.random())
+        lista_estimativas += [soma/n]
+    
+    plt.figure(num=0,dpi=120)    
+    plt.plot(list(range(len(lista_estimativas))),lista_estimativas)
+    plt.yticks(np.arange(min(lista_estimativas), max(lista_estimativas), 0.1))
+    
+    return lista_estimativas[N-1]
+
+
+def hit_or_miss(n = 4900000, Seed = None):
+    random.seed(Seed)
+    """
+    Esta funcao deve retornar a sua estimativa para o valor da integral de f(x)
+    usando o metodo hit or miss
+    Escreva o seu codigo nas proximas linhas
+    """
+    n = n
+    ponto_dentro = 0
+    lista_estimativas = []
+    total = 1
+    
+    #gerando pontos uniformemente dentro do quadrado [0,1]x[0,1] aplicando a função e checando se está dentro ou nao
+    for i in range(1,n+1):
+        ponto = [random.random(), random.random()]
+        if ponto[1] <= f(ponto[0]) :
+            ponto_dentro += 1
+            lista_estimativas += [ponto_dentro/total]
+        
+        else:
+            None
+        total += 1
+
+    plt.figure(num=0,dpi=120)    
+    plt.plot(list(range(len(lista_estimativas))),lista_estimativas)
+    plt.yticks(np.arange(min(lista_estimativas), max(lista_estimativas), 0.1))
+
+    return ponto_dentro/n
+
+
+def control_variate(n = 120000, Seed = None):
+    random.seed(Seed)
+    """
+    Esta funcao deve retornar a sua estimativa para o valor da integral de f(x)
+    usando o metodo control variate
+    Escreva o seu codigo nas proximas linhas
+    """
+    
+    N = n
+    estimativa = 0
+    lista_estimativas = []
+    
+    #para este método utilizamos a função g(x) que aproxima bem a função f(x)
+    for i in range(1,N+1):
+        x = random.random()
+        estimativa += (f(x)-g(x)+0.75)
+        lista_estimativas += [estimativa/i]
+        
+    
+    plt.figure(num=0,dpi=120)    
+    plt.plot(list(range(len(lista_estimativas))),lista_estimativas)
+    plt.yticks(np.arange(min(lista_estimativas), max(lista_estimativas), 0.1))
+         
+
+    return lista_estimativas[N-1]
+
+
+def importance_sampling(n = 200000, Seed = None):
+    random.seed(Seed)
+    """
+    Esta funcao deve retornar a sua estimativa para o valor da integral de f(x)
+    usando o metodo importance sampling
+    Escreva o seu codigo nas proximas linhas
+    """
+    N = n
+    estimativa = 0
+    lista_estimativas = []
+    dist = scipy.stats.beta(0.99,1.27)
+    
+    #para esta função utilizaremos a distribuição beta com parametros 0.99 e 1.27
+    for i in range(1,N+1):
+        x = np.random.beta(0.99,1.27)
+        estimativa += f(x)/dist.pdf(x)
+        lista_estimativas += [estimativa/i]
+        #print(f'O progresso está {round((i/N)*100,2)}% completo')
+        #descomente a linha acima se o número de pontos passar de 10 mil pois a função demora um pouco
+    return lista_estimativas[N-1]
+
+
+def plot_fx():
+    """
+    função para plotar o gráfico da função f(x)
+    
+    """
+    xs = [float(i/100) for i in range(int(100))]
+    ys = [f(x) for x in xs]
+    plt.figure(dpi=120)
+    plt.plot(xs,ys)
+    plt.title("f(x)");
+
+
+def plot_hist():
+    """
+    função para plotar o histogramas de 1000 estimativas dos métodos de Monte-Carlo
+    
+    """
+    lista = []
+    for i in range(1000):
+        lista += [control_variate()]
+        print(f'O progresso está {round((i/1000)*100,2)}% completo')
+        
+    plt.figure(dpi=120)
+    plt.hist((lista))
+    print(np.mean(lista))
+    print(np.std(lista))
+    
+def encontre_n():
+    
+    """
+    Função para encontrar o N ideal
+    Troque a chamada do método pelo desejado
+    Pode ser um pouco lenta dependendo do método
+    """
+    #N inicial
+    n=1
+    #desvio padrao inicial
+    s=0
+    erro1 = 1
+    lista_estimativas = []
+    tolerancia = 0.0005
+    #troque abaixo para diferentes métodos
+    estimativa = control_variate(n)
+    lista_estimativas += [estimativa]
+    while erro1 > tolerancia:
+        n += 10000
+        #troque abaixo para outro método
+        estimativa = control_variate(n)
+        lista_estimativas += [estimativa]
+        m = np.mean(lista_estimativas)
+        s = np.std(lista_estimativas)
+        #cálculo do erro para o N setado e o desvio-padrão do estimador com N pontos
+        erro2 = (1.96**2 * s)/np.sqrt(n)
+        erro1 = 2*erro2/m
+    return n
+
+def main():
+    #Coloque seus testes aqui
+    print(crude())
+    print(hit_or_miss())
+    print(control_variate())
+    print(importance_sampling())
+
+
+#if __name__ == "__main__":
+#   main()
